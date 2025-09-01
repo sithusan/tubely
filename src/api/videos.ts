6df39a9bf05eae065f6b8e42dbcf21ad149aee01
Si Thu San <sithusanss@gmail.com>
2025-09-01 13:03:46 +0700

    feat: video upload

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,8 +1,66 @@
 import { respondWithJSON } from "./json";
-
 import { type ApiConfig } from "../config";
 import type { BunRequest } from "bun";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { getBearerToken, validateJWT } from "../auth";
+import { getVideo, updateVideo } from "../db/videos";
+import { randomBytes } from "node:crypto";
+import path from "node:path";
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
+  const { videoId } = req.params as { videoId?: string };
+
+  if (videoId === undefined) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  const formData = await req.formData();
+
+  const file = formData.get("video");
+
+  if (!(file instanceof File)) {
+    throw new BadRequestError("video file missing");
+  }
+
+  const allowFileTypes = ["video/mp4"];
+  const fileMimeType = file.type;
+
+  if (!allowFileTypes.includes(fileMimeType)) {
+    throw new BadRequestError("Mimetype not allowed");
+  }
+
+  const MAX_UPLOAD_SIZE = 1 << 30; // 1GB
+
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Video file size if large than max upload size");
+  }
+
+  const video = getVideo(cfg.db, videoId);
+
+  if (video === undefined) {
+    throw new NotFoundError("Couldn't find video");
+  }
+
+  if (video.userID !== userID) {
+    throw new UserForbiddenError("Forbidden");
+  }
+
+  const fileName = `${randomBytes(32).toString("base64url")}.${
+    fileMimeType.split("/")[1]
+  }`;
+
+  const filePath = path.join(cfg.assetsRoot, fileName);
+
+  Bun.write(filePath, await file.arrayBuffer());
+
+  cfg.s3Client.write(fileName, Bun.file(filePath));
+
+  video.videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${fileName}`;
+
+  await updateVideo(cfg.db, video);
+
   return respondWithJSON(200, null);
 }
